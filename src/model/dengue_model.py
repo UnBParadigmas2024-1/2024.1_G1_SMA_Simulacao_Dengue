6e4e4d6635b218cf810372af5680e64f41aed19b
diogjunior100 <diogjunior10071@gmail.com>
2024-08-10 11:27:24 -0300

    Adiciona alteracoes no arquivo do modelo de contaminacao

--- src/model/dengue_model.py
+++ src/model/dengue_model.py
@@ -10,8 +10,20 @@ class DengueContaminationModel(Model):
     def __init__(self, width, height, initial_people, initial_mosquitoes, initial_water):
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
+
+        self.num_people = initial_people
+        self.num_mosquitoes = initial_mosquitoes
+
         self.datacollector = DataCollector(
-            agent_reporters={"State": lambda a: a.state, "Infections": lambda a: getattr(a, "infections", 0)}
+            #agent_reporters={"State": lambda a: a.state, "Infections": lambda a: getattr(a, "infections", 0)}
+            model_reporters={
+                "Total People": lambda m: m.num_people,
+                "Total Mosquitoes": lambda m: m.num_mosquitoes
+            },
+            agent_reporters={
+                "State": lambda a: getattr(a, "state", None), 
+                "Infections": lambda a: getattr(a, "infections", 0)
+            }
         )
         
         for i in range(initial_people):
@@ -33,3 +45,6 @@ class DengueContaminationModel(Model):
     def step(self):
         self.datacollector.collect(self)
         self.schedule.step()
+
+        self.num_people = sum(1 for agent in self.schedule.agents if isinstance(agent, PersonAgent))
+        self.num_mosquitoes = sum(1 for agent in self.schedule.agents if isinstance(agent, MosquitoAgent))
