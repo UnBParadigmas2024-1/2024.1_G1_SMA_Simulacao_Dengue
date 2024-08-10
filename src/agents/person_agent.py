fd67d718e6e285ed047bb8563576d27f6c08f16f
Lucas Macedo Barboza <64804763+Luckx98@users.noreply.github.com>
2024-08-10 15:14:40 +0000

    feat: Adiçao de agente de pessoas
    Co-authored-by: Abraao1231<abraao1231@gmail.com>
    Co-authored-by: frmiza<mizzaelsantos1@gmail.com>

--- src/agents/person_agent.py
+++ src/agents/person_agent.py
@@ -1,33 +1,42 @@
 from mesa import Agent
 
+# Definindo os estados de saúde
+HEALTHY = "Saudável"
+DENGUE = "Dengue"
+SEVERE_DENGUE = "Dengue Hemorrágica"
+DEAD = "Morto"
+
 class PersonAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.state = "Saudável"
         self.infections = 0
-    
+
+
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
-    
-    def step(self):
-        self.move()
+
+    def interact_with_water(self):
         cell_contents = self.model.grid.get_cell_list_contents([self.pos])
-        water = [obj for obj in cell_contents if isinstance(obj, WaterObject)]
-        if water:
-            self.model.grid.remove_agent(water[0])
+        for content in cellmates:
+            if isinstance(content,  self.model.AguaParadaAgent):
+                self.model.grid.remove_agent(content)
+                print(f"Água parada eliminada por agente {self.unique_id}")
+
+    def step(self):
+        if self.state != DEAD:
+            self.move()
+            self.interact_with_water()
+        cell_contents = self.model.grid.get_cell_list_contents([self.pos])
+        # water = [obj for obj in cell_contents if isinstance(obj, WaterObject)]
+        # if water:
+        #     self.model.grid.remove_agent(water[0])
         
-        mosquitoes = [obj for obj in cell_contents if isinstance(obj, MosquitoAgent)]
-        for mosquito in mosquitoes:
-            if mosquito.state == "Infectado":
-                self.infect()
+        # mosquitoes = [obj for obj in cell_contents if isinstance(obj, MosquitoAgent)]
+        # for mosquito in mosquitoes:
+        #     if mosquito.state == "Infectado":
+        #         self.infect()
     
-    def infect(self):
-        if self.state == "Saudável":
-            self.state = "Dengue"
-            self.infections += 1
-        elif self.state == "Dengue":
-            self.state = "Dengue Hemorrágica"
-        elif self.state == "Dengue Hemorrágica":
-            self.state = "Morto"
+
