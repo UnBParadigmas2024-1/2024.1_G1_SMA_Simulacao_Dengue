5c64e85dc2627c0284c43aae961b373d56e17852
frmiza <mizzaelsantos1@gmail.com>
2024-08-10 19:13:01 -0300

    Add interaction with mosquitoes Co-authored-by: Abraao1231<abraao1231@gmail.com> Co-authored-by: LucasMacedo<lucasmacedob@gmail.com>

--- src/agents/person_agent.py
+++ src/agents/person_agent.py
@@ -1,6 +1,8 @@
 from mesa import Agent
 
-# Definindo os estados de saúde
+from agents.water_object import WaterObject
+from agents.mosquito_agent import MosquitoAgent
+
 HEALTHY = "Saudável"
 DENGUE = "Dengue"
 SEVERE_DENGUE = "Dengue Hemorrágica"
@@ -9,34 +11,40 @@ DEAD = "Morto"
 class PersonAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.state = "Saudável"
+        self.state = HEALTHY
         self.infections = 0
-
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
-
+    
     def interact_with_water(self):
-        cell_contents = self.model.grid.get_cell_list_contents([self.pos])
-        for content in cellmates:
-            if isinstance(content,  self.model.AguaParadaAgent):
-                self.model.grid.remove_agent(content)
-                print(f"Água parada eliminada por agente {self.unique_id}")
+        neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False)
+        water_objects = [agent for agent in neighbors if isinstance(agent, WaterObject)]
+        for water in water_objects:
+            water.remove_standing_water()
+            break
+    
+    def interact_with_mosquitoes(self):
+        neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False)
+        mosquitoes_objects = [agent for agent in neighbors if isinstance(agent, MosquitoAgent)]
+        for mosquitoes in mosquitoes_objects:
+            if mosquitoes.state == "Infectado":
+                self.infect()
+    
 
     def step(self):
         if self.state != DEAD:
             self.move()
             self.interact_with_water()
-        cell_contents = self.model.grid.get_cell_list_contents([self.pos])
-        # water = [obj for obj in cell_contents if isinstance(obj, WaterObject)]
-        # if water:
-        #     self.model.grid.remove_agent(water[0])
-        
-        # mosquitoes = [obj for obj in cell_contents if isinstance(obj, MosquitoAgent)]
-        # for mosquito in mosquitoes:
-        #     if mosquito.state == "Infectado":
-        #         self.infect()
-    
+            self.interact_with_mosquitoes()
 
+    def infect(self):
+        if self.state == HEALTHY:
+            self.state = DENGUE
+            self.infections += 1
+        elif self.state == DENGUE:
+            self.state = SEVERE_DENGUE
+        elif self.state == SEVERE_DENGUE:
+            self.state = DEAD
