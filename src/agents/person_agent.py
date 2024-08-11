b6f7e2652f10665d07c2eab5a6e3e05178b8b24b
phrezende-eng <ph.hr.001@gmail.com>
2024-08-11 12:57:36 -0300

    feat: person infect mosquito and remove code

--- src/agents/person_agent.py
+++ src/agents/person_agent.py
@@ -1,32 +1,37 @@
+from mosquito_agent import MosquitoAgent
 from mesa import Agent
+
 
 class PersonAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.state = "Saudável"
         self.infections = 0
-    
+
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
-            self.pos,
-            moore=True,   # Considera todas as 8 direções
-            include_center=False)
-        
+            self.pos, moore=True, include_center=False  # Considera todas as 8 direções
+        )
+
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
-    
+
     def step(self):
         self.move()
         cell_contents = self.model.grid.get_cell_list_contents([self.pos])
+
+        mosquitoes = [obj for obj in cell_contents if isinstance(obj, MosquitoAgent)]
+        for mosquito in mosquitoes:
+            if (
+                self.state in ["Dengue", "Dengue Hemorrágica"]
+                and mosquito.state == "Não infectado"
+            ):
+                mosquito.state = "Infectado"
+
         # water = [obj for obj in cell_contents if isinstance(obj, WaterObject)]
         # if water:
         #     self.model.grid.remove_agent(water[0])
-        
-        # mosquitoes = [obj for obj in cell_contents if isinstance(obj, MosquitoAgent)]
-        # for mosquito in mosquitoes:
-        #     if mosquito.state == "Infectado":
-        #         self.infect()
-    
+
     def infect(self):
         if self.state == "Saudável":
             self.state = "Dengue"
