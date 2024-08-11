91117c8294695f6c84645df07c4afe4b72f5056d
Mateus <mateus.frauzino@gmail.com>
2024-08-11 20:48:16 -0300

    fix: correção de imports circulares
    Co-authored-by: PauloAbiAcl<pv.silva23@hotmail.com>

--- src/visualization/portrayal.py
+++ src/visualization/portrayal.py
@@ -1,34 +1,41 @@
-from agents.person_agent import PersonAgent
+import mesa
 from agents.mosquito_agent import MosquitoAgent
+from agents.person_agent import PersonAgent
 from agents.water_object import WaterObject
 
-def agent_portrayal(agent):
-    if isinstance(agent, PersonAgent):
+def getColorPerson(agent):
+    if agent.state == "Saudável":
+        return "green"
+    elif agent.state == "Dengue":
+        return "yellow"
+    elif agent.state == "Dengue Hemorrágica":
+        return "red"
+    else:
+        return "black"
+
+def getPortrayalPerson(agent):
+    return {
+        "Shape": "circle",
+        "Filled": "true",
+        "Layer": 0,
+        "Color": getColorPerson(agent),
+        "r": 0.5,
+        "id": f"Pessoa {agent.unique_id}"
+    }
+
+def circle_portrayal_example(agent):
+    if agent is None:
+        return
+
+    if isinstance(agent, MosquitoAgent):
         portrayal = {
             "Shape": "circle",
             "Filled": "true",
-            "r": 0.8,
-            "Layer": 1  
+            "Layer": 0,
+            "Color": "#964b00",
+            "r": 0.5,
+            "id": f"Mosquito {agent.unique_id} {agent.state}"
         }
-        if agent.state == "Saudável":
-            portrayal["Color"] = "green"
-        elif agent.state == "Dengue":
-            portrayal["Color"] = "yellow"
-        elif agent.state == "Dengue Hemorrágica":
-            portrayal["Color"] = "red"
-        else:
-            portrayal["Color"] = "black"
-            portrayal["r"] = 0.5
-    
-    elif isinstance(agent, MosquitoAgent):
-        portrayal = {
-            "Shape": "rect",
-            "Filled": "true",
-            "w": 0.5, "h": 0.5,
-            "Color": "gray" if agent.state == "Não infectado" else "orange",
-            "Layer": 2  
-        }
-    
     elif isinstance(agent, WaterObject):
         portrayal = {
             "Shape": "circle",
@@ -44,15 +51,15 @@ def agent_portrayal(agent):
     return portrayal
 
 canvas_element = mesa.visualization.CanvasGrid(
-    circle_portrayal_example, 20, 20, 700, 700
+    circle_portrayal_example, 10, 10, 700, 700
 )
 
 model_kwargs = {
-    "initial_people": mesa.visualization.Slider(name="Quantidade de pessoas", min_value=0, max_value=10, step=1, value=1),
-    "initial_mosquitoes": mesa.visualization.Slider(name="Quantidade de mosquitos", min_value=0, max_value=10, step=1, value=1),
-    "initial_water": mesa.visualization.Slider(name="Quantidade de águas paradas", min_value=0, max_value=10, step=1, value=1),
-    "width": 20,
-    "height": 20
+    "initial_people": mesa.visualization.Slider(name="Quantidade de pessoas", min_value=0, max_value=20, step=1, value=10),
+    "initial_mosquitoes": mesa.visualization.Slider(name="Quantidade de mosquitos", min_value=0, max_value=20, step=1, value=5),
+    "initial_water": mesa.visualization.Slider(name="Quantidade de águas paradas", min_value=0, max_value=20, step=1, value=5),
+    "width": 10,
+    "height": 10
 }
 
 # Gráfico para a contagem de mosquitos
